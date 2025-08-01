80c3a01bd6ba0e62c699782ec53303eae6d38202
Abhi Mahajan <abhishekmahajan8285@gmail.com>
2025-08-02 01:34:47 +0530

    fronteend code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router";
 import React, { Suspense } from "react";
-import signUp from "./pages/signup.jsx";
+import SignUp from "./pages/signup.jsx";
+import Login from "./pages/login.jsx";
 import "./index.css";
 import Loading from "./component/loading.jsx";
 
@@ -9,8 +10,8 @@ const App = () => {
     <div className="main--container">
       <Suspense fallback={<Loading />}>
         <Routes>
-          <Route path="/signup" element={<signUp />} />
-          {/* <Route path="/signup" element={<signup />} /> */}
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Login />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const login = () => {
+  return <div className="text-3xl font-bold underline">login</div>;
+};
+
+export default login;
