2b885c9abf14ea8eaf6aef1bb009480063ad196b
Abhi Mahajan <abhishekmahajan8285@gmail.com>
2025-08-02 02:46:27 +0530

    vercel 3

--- Backend/index.js
+++ Backend/index.js
@@ -29,3 +29,36 @@ const startServer = async () => {
 };
 
 startServer();
+
+
+// import express from "express";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import authRoutes from "./routes/authRoutes.js";
+// import bookRoutes from "./routes/bookRoutes.js";
+// import { connectDB } from "./dbConnection/db.js";
+
+// dotenv.config();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/books", bookRoutes);
+
+// // Connect to DB only once
+// let isConnected = false;
+
+// const handler = async (req, res) => {
+//   if (!isConnected) {
+//     await connectDB();
+//     isConnected = true;
+//   }
+
+//   // Let express handle the request
+//   return app(req, res);
+// };
+
+// export default handler;
